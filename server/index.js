7267d1c712ab1f919b62f52c1ac209b971e2d828
Shivang Garg <shivang10garg@gmail.com>
2022-02-10 23:23:47 +0530

    🗃️ configures database with the server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,27 @@
 import bodyParser from "body-parser";
 import cors from "cors";
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
+
+dotenv.config();
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+
+mongoose.connect(
+    `mongodb+srv://health-app:${process.env.MONGODBPASSWORD}@cluster0.ovly9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
+
+mongoose.connection.once("open", () => {
+    console.log("connected to database");
+});
 
 app.get("/", (req, res) => {
     res.send("server is running");
