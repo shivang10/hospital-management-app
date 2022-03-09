efc4eb2d6bad6f944d4c01ac6ac1a7d14d48a983
Shivang Garg <shivang10garg@gmail.com>
2022-03-09 09:12:18 +0530

    ✨ adds user's dashboard and profile page

---
+++ client/src/profile/doctorProfile.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DoctorProfile: React.FC = () => {
+    return (
+        <div>
+            Doctor Profile
+        </div>
+    );
+};
+
+export default DoctorProfile;
--- client/src/routes/routeLinks.ts
+++ client/src/routes/routeLinks.ts
@@ -1,3 +1,5 @@
 export const loginRouteLink = "/login";
 export const signUpRouteLink = "/signUp";
 export const homepageRouteLink = "/";
+export const dashboardRouteLink = "/dashboard";
+export const myProfileRouteLink = "/my-profile";
