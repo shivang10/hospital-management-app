a551f795943a48be5e783c1f702dbeaac880069b
Shivang Garg <shivang10garg@gmail.com>
2022-03-09 23:48:38 +0530

    💄 shows username on navbar

--- client/src/navbar/navbar.tsx
+++ client/src/navbar/navbar.tsx
@@ -4,21 +4,20 @@ import {FaUserAlt} from "react-icons/fa";
 import {Link} from "react-router-dom";
 
 import {myProfileRouteLink} from "../routes/routeLinks";
-
+import {getUserDetails} from "../utils/tokenDetails";
 
 const Navbar: React.FC = () => {
+    const {username} = getUserDetails();
+
     return (
         <div className="navbar">
-            <div>
-                My-Health
-            </div>
-            <div>
-                Search
-            </div>
+            <div>My-Health</div>
+            <div>Search</div>
             <div className="navbar__right">
                 <Link
+                    className="navbar__right-link"
                     to={myProfileRouteLink}>
-                    <FaUserAlt className="profile-icon"/>
+                    <FaUserAlt className="profile-icon"/>{username}
                 </Link>
             </div>
         </div>
